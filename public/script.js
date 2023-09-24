e1fe978b587ba29a7e7d14beaad5ac02e361458e
spicaz <developerbdg@gmail.com>
2023-09-24 13:35:30 +0700

    update darkmode

--- public/script.js
+++ public/script.js
@@ -1,20 +1,43 @@
 // hamburger & nav menu
-const hamburger = document.querySelector('#hamburger');
-const navMenu = document.querySelector('#nav-menu');
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
 
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('hamburger-active')
-  navMenu.classList.toggle('hidden')
-})
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// klik diluar hamburger
+window.addEventListener("click", (e) => {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
 
 // navbar header
 window.onscroll = () => {
-  const header = document.querySelector('header')
+  const header = document.querySelector("header");
   const fixNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.scrollY > fixNav) {
-    header.classList.add('header-fixed')
+    header.classList.add("header-fixed");
+    toTop.classList.add("flex");
+    toTop.classList.remove("hidden");
   } else {
-    header.classList.remove('header-fixed')
+    header.classList.remove("header-fixed");
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
   }
-}
+};
+
+// darkmode
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", () => {
+  darkToggle.checked
+    ? html.classList.add("dark")
+    : html.classList.remove("dark");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./index.html"],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
-      padding: '16px',
+      padding: "16px",
     },
     extend: {
       colors: {
-        primary: '#D0BFFF',
-        secondary: '#917FB3',
-        baseC: '#DFCCFB',
-        darkbg: '#2A2F4F',
-        blogBg: '#FFF3DA',
+        primary: "#D0BFFF",
+        secondary: "#917FB3",
+        baseC: "#DFCCFB",
+        darkbg: "#2A2F4F",
+        blogBg: "#FFF3DA",
       },
       screens: {
-        '2xl': '1320px'
-      }
+        "2xl": "1320px",
+      },
     },
   },
   plugins: [],
-}
-
+};
